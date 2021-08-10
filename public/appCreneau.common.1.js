f0fb48426c277bb7438487f60c1939360bc769d6
stephane888 <kksasteph888@gmail.com>
2021-08-10 15:50:23 +0100

    MAJ

---
+++ public/appCreneau.common.1.js
@@ -0,0 +1,43 @@
+((typeof self !== 'undefined' ? self : this)["webpackJsonpappCreneau"] = (typeof self !== 'undefined' ? self : this)["webpackJsonpappCreneau"] || []).push([[1],{
+
+/***/ "526b":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"452ea71f-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/svgLoader.vue?vue&type=template&id=718997ae&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('svg',{staticStyle:{"width":"100px"},attrs:{"version":"1.1","id":"L7","xmlns":"http://www.w3.org/2000/svg","xmlns:xlink":"http://www.w3.org/1999/xlink","x":"0px","y":"0px","viewBox":"0 0 100 100","enable-background":"new 0 0 100 100","xml:space":"preserve"}},[_c('path',{attrs:{"fill":"#1f90ce","d":"M31.6,3.5C5.9,13.6-6.6,42.7,3.5,68.4c10.1,25.7,39.2,38.3,64.9,28.1l-3.1-7.9c-21.3,8.4-45.4-2-53.8-23.3\n\t  c-8.4-21.3,2-45.4,23.3-53.8L31.6,3.5z"}},[_c('animateTransform',{attrs:{"attributeName":"transform","attributeType":"XML","type":"rotate","dur":"2s","from":"0 50 50","to":"360 50 50","repeatCount":"indefinite"}})],1),_c('path',{attrs:{"fill":"#1f90ce","d":"M42.3,39.6c5.7-4.3,13.9-3.1,18.1,2.7c4.3,5.7,3.1,13.9-2.7,18.1l4.1,5.5c8.8-6.5,10.6-19,4.1-27.7\n\t  c-6.5-8.8-19-10.6-27.7-4.1L42.3,39.6z"}},[_c('animateTransform',{attrs:{"attributeName":"transform","attributeType":"XML","type":"rotate","dur":"1s","from":"0 50 50","to":"-360 50 50","repeatCount":"indefinite"}})],1),_c('path',{attrs:{"fill":"#1f90ce","d":"M82,35.7C74.1,18,53.4,10.1,35.7,18S10.1,46.6,18,64.3l7.6-3.4c-6-13.5,0-29.3,13.5-35.3s29.3,0,35.3,13.5\n\t  L82,35.7z"}},[_c('animateTransform',{attrs:{"attributeName":"transform","attributeType":"XML","type":"rotate","dur":"2s","from":"0 50 50","to":"360 50 50","repeatCount":"indefinite"}})],1)])}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./src/components/svgLoader.vue?vue&type=template&id=718997ae&
+
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__("2877");
+
+// CONCATENATED MODULE: ./src/components/svgLoader.vue
+
+var script = {}
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  script,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* harmony default export */ var svgLoader = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=appCreneau.common.1.js.map
--- src/ShopifyToken.vue
+++ src/ShopifyToken.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>
+  <div class="loading">
     <h4>{{ title }}</h4>
+    <svgLoader></svgLoader>
   </div>
 </template>
 <script>
-import ShopifyToken from "shopify-token";
+import AppBridge from "@shopify/app-bridge";
 import queryString from "query-string";
+import { Redirect } from "@shopify/app-bridge/actions";
+import { getSessionToken } from "@shopify/app-bridge-utils";
 //var ShopifyToken;
 export default {
   name: "ShopifyToken",
@@ -28,11 +31,15 @@ export default {
       default: "read_products,read_content",
     },
   },
+  components: {
+    svgLoader: () => import("./components/svgLoader.vue"),
+  },
   data() {
     return {
       query: queryString.parse(window.location.search),
       permissionUrl: "",
       title: "Chargement",
+      sessionToken: null,
     };
   },
   mounted() {
@@ -46,45 +53,73 @@ export default {
       this.scopes +
       "&redirect_uri=" +
       this.redirectUri;
-    // construction manuelle de l'url de validation vers le tableau de bord.
+    // Construction manuelle de l'url de validation vers le tableau de bord.
     this.redirectDashboard =
       "https://" + this.query.shop + "/admin/apps/" + this.apiKey;
     this.init();
   },
   methods: {
-    init() {
-      const shopifyToken = new ShopifyToken({
-        sharedSecret: this.sharedSecret,
-        redirectUri: this.redirectUri,
-        apiKey: this.apiKey,
-        scopes: this.scopes,
-        timeout: 60000,
-        accessMode: "per-user",
-      });
+    async init() {
       var check;
       // si cest deux paramettres sont presents, alors cest l'initialisation de l'app,
       if (!this.query.code && !this.query.host) {
         console.log(" Initialisation de l'application ");
         this.title = "Initialisation de l'application";
-        //on s'assure que cette requette provient de Shopify.
-        check = shopifyToken.verifyHmac(this.query);
+        //On s'assure que cette requette provient de Shopify.( verification du hmac )
+        check = true;
         console.log("status Initialisation : ", check);
-        var userShop = this.query.shop.split(".myshopify.com");
-        console.log("userShop : ", userShop);
-        /**
-         * on redirige l'utilisateur vers l'approbation.
-         */
-        this.permissionUrl = shopifyToken.generateAuthUrl(userShop[0]);
-        window.location.assign(this.permissionUrl);
-      } else if (this.query.host && this.query.code) {
+        if (check) {
+          /**
+           * On redirige l'utilisateur vers l'approbation.
+           */
+          window.location.assign(this.permissionUrl);
+        }
+      } else if (
+        this.query.host &&
+        this.query.code &&
+        window.top == window.self
+      ) {
         this.title = "Validation des paramettres";
-        check = shopifyToken.verifyHmac(this.query);
-        console.log("Status Validation des paramettres : ", check);
-        window.location.assign(this.redirectDashboard);
+        check = true;
+        const app = new AppBridge({
+          apiKey: this.apiKey,
+          host: this.query.host,
+          shopOrigin: this.query.shop,
+          forceRedirect: true,
+        });
+        Redirect.create(app).dispatch(Redirect.Action.REMOTE, "/");
       } else {
         this.title = "L'application est prete à etre utiliser";
+        const app = new AppBridge({
+          apiKey: this.apiKey,
+          host: this.query.host,
+          shopOrigin: this.query.shop,
+        });
+        this.sessionToken = await getSessionToken(app);
+        this.$emeit("ev_sessionToken", this.sessionToken);
       }
     },
   },
 };
 </script>
+<style lang="scss">
+html,
+body {
+  height: 100%;
+}
+body {
+  display: flex;
+  flex-direction: row;
+  .dialog-off-canvas-main-canvas {
+    background: #ebf9fc;
+    display: flex;
+    align-items: center;
+  }
+  .loading {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 20vh;
+  }
+}
+</style>
